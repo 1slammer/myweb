383f5da37ac792bd1b4e60bd4b200e5bc9781221
1slammer <jzn0016@auburn.edu>
2015-05-17 20:42:48 -0500

    Initial Commit

---
+++ js/my-webpage2.js
@@ -0,0 +1,9 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+greeting = "I am a senior"
+			+ " in Software Engineering at Auburn University. Please feel free "
+			+ "to explore this site to find out more about me."
+
+document.write('<h4>' + greeting + '</h4>');
